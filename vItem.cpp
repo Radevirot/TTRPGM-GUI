5d822bbbacad4e9c90bc7e1a485e5d0597f2c16a
Zike420 <78177106+Zike420@users.noreply.github.com>
2021-02-16 18:38:36 -0300

    a
    a

--- vItem.cpp
+++ vItem.cpp
@@ -12,6 +12,21 @@ vItem::~vItem() {
 void vItem::OnClickAplicar( wxCommandEvent& event )  {
 	Item I;
 	I.NombrarItem(wx_to_std(m_NombreItem->GetLabel()));
+	I.ModificarCant(m_Cantidad->GetValue());
+	I.ModificarStat(0,(m_PuntosVida->GetValue()));
+	I.ModificarStat(1,(m_Defenza->GetValue()));
+	I.ModificarStat(2,(m_Fuerza->GetValue()));
+	I.ModificarStat(3,(m_Agilidad->GetValue()));
+	I.ModificarStat(4,(m_ResistenciaMagica->GetValue()));
+	I.ModificarStat(5,(m_Inteligencia->GetValue()));
+	I.ModificarStat(6,(m_Mana->GetValue()));
+	I.ModificarStat(7,(m_Danio->GetValue()));
+	I.ModificarStat(8,(m_Rango->GetValue()));
+	I.ModificarStat(9,(m_Punteria->GetValue()));
+	I.ModificarStat(10,(m_Bloqueo->GetValue()));
+	I.ModificarDetalle(wx_to_std(m_Detalle->GetLabel()));
+	I.ModificarDesc(wx_to_std(m_Descripcion->GetLabel()));
+	m_partida->AgregarItem(I);
 }
 
 void vItem::OnClickExportar( wxCommandEvent& event )  {
