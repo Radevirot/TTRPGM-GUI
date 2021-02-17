7f54a55c9d3cea3f8a74ba65355f99f6e5d4051a
Zike420 <78177106+Zike420@users.noreply.github.com>
2021-02-17 19:20:24 -0300

    Combate y personaje
    a

--- vCombate.cpp
+++ vCombate.cpp
@@ -1,6 +1,25 @@
 #include "vCombate.h"
 
-vCombate::vCombate(wxWindow *parent) : Ventana_combate(parent) {
+vCombate::vCombate(wxWindow *parent, Partida *p) : Ventana_combate(parent) {
+	m_partida=p;
+	m_Formula->Append(std_to_wx("(50+(DÑ*4)*MULT)-(DFN*2)"));
+	m_Formula->Append(std_to_wx("(((DÑ*0.5)*MULT)-(DFN*0.25))"));
+	m_Formula->Append(std_to_wx("(((FRZ)*MULT)-(DFN*0.5))"));
+	m_Formula->Append(std_to_wx("((FRZ+(DÑ*0.5)*MULT)-(DFN*0.25))"));
+	m_Formula->Append(std_to_wx("((FRZ+(DÑ*0.5)*MULT)-(DFN*0.5))"));
+	m_Formula->SetSelection(0);
+	int Tam=m_partida->ObtenerTamPersonajes();
+	for(int i=0;i<Tam;i++) { 
+		Personaje P=m_partida->ObtenerPersonaje(i);
+		m_Atacante->Append(std_to_wx((P.ObtenerNombre())+"-"+std::to_string(P.ObtenerStat(0))));
+		m_Receptor->Append(std_to_wx((P.ObtenerNombre())+"-"+std::to_string(P.ObtenerStat(0))));
+	}
+	m_Atacante->SetSelection(0);
+	if(Tam>1){ 
+		m_Receptor->SetSelection(1);
+	}else{ 
+		m_Receptor->SetSelection(0);
+	}
 	Show();
 }
 
@@ -13,7 +32,10 @@ void vCombate::OnClickCerrar( wxCommandEvent& event )  {
 }
 
 void vCombate::OnClickAtacar( wxCommandEvent& event )  {
-	event.Skip();
+	int posatc=m_Atacante->GetSelection();
+	int posrecp=m_Receptor->GetSelection();
+	int Danio=m_partida->Combate(posatc,posrecp,m_Formula->GetSelection(),m_Multiplicador->GetValue(),m_ModificarPV->IsChecked());
+	m_DanioProvo->SetLabel(std_to_wx(std::to_string(Danio)));
 }
 
 
--- vCombate.h
+++ vCombate.h
@@ -1,17 +1,20 @@
 #ifndef VCOMBATE_H
 #define VCOMBATE_H
 #include "wxfb_project.h"
+#include "Partida.h"
+#include "string_conv.h"
+#include "Personaje.h"
 
 class vCombate : public Ventana_combate {
 	
 private:
-	
+	Partida *m_partida;
 protected:
 	void OnClickAtacar( wxCommandEvent& event )  override;
 	void OnClickCerrar( wxCommandEvent& event )  override;
 	
 public:
-	vCombate(wxWindow *parent=NULL);
+	vCombate(wxWindow *parent, Partida *p);
 	~vCombate();
 };
 
