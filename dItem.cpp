8841be6891432844d8cca2d6219d95712a023999
Zike420 <78177106+Zike420@users.noreply.github.com>
2021-02-19 18:54:30 -0300

    a
    A

--- dItem.cpp
+++ dItem.cpp
@@ -1,11 +1,28 @@
 #include "dItem.h"
 
-dItem::dItem(wxWindow *parent) : Dialogo_item(parent) {
-	
+dItem::dItem(wxWindow *parent, Partida *p) : Dialogo_item(parent) {
+	m_partida=p;
+	Show();
 }
 
 void dItem::OnClickAplicar( wxCommandEvent& event )  {
-	event.Skip();
+	Item I;
+	I.NombrarItem(wx_to_std(m_NombreItem->GetValue()));
+	I.ModificarCant(m_Cantidad->GetValue());
+	I.ModificarStat(0,(m_PuntosVida->GetValue()));
+	I.ModificarStat(1,(m_Defenza->GetValue()));
+	I.ModificarStat(2,(m_Fuerza->GetValue()));
+	I.ModificarStat(3,(m_Agilidad->GetValue()));
+	I.ModificarStat(4,(m_ResistenciaMagica->GetValue()));
+	I.ModificarStat(5,(m_Inteligencia->GetValue()));
+	I.ModificarStat(6,(m_Mana->GetValue()));
+	I.ModificarStat(7,(m_Danio->GetValue()));
+	I.ModificarStat(8,(m_Rango->GetValue()));
+	I.ModificarStat(9,(m_Punteria->GetValue()));
+	I.ModificarStat(10,(m_Bloqueo->GetValue()));
+	I.ModificarDetalle(wx_to_std(m_Detalle->GetValue()));
+	I.ModificarDesc(wx_to_std(m_Descripcion->GetValue()));
+	m_partida->AgregarItem(I);
 }
 
 void dItem::OnClickExportar( wxCommandEvent& event )  {
@@ -15,4 +32,3 @@ void dItem::OnClickExportar( wxCommandEvent& event )  {
 dItem::~dItem() {
 	
 }
-
