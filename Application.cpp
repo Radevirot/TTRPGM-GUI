26c97945dd4297a0d63e8ca12746a230592002d5
Radevirot <78053632+Radevirot@users.noreply.github.com>
2021-02-22 20:39:11 -0300

    f
    f

--- Application.cpp
+++ Application.cpp
@@ -6,14 +6,15 @@
 IMPLEMENT_APP(Application)
 
 bool Application::OnInit() {
-	wxIcon *icon = new wxICON("imagenes/logo.ico");
 	m_partida = new Partida("");
+	wxIcon *icon = new wxICON();
+	icon->LoadFile("D:\\Repositorios\\OdioMiVida\\TTRPGM-GUI\\imagenes\\logo.ico");
 	wxInitAllImageHandlers();
 	vPrincipal Princ(m_partida);
 	int valor = Princ.ShowModal();
 	if(valor==1){
 		vPartida *Part = new vPartida(NULL,m_partida);
-		Part->SetIcon(wxICON(icon));
+		Part->SetIcon(wxICON(0));
 	}else{
 		return false;
 	}
