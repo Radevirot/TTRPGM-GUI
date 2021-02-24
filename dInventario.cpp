98ff94dd01d4fec1979b81ec6e9e2ae78251cb28
Radevirot <78053632+Radevirot@users.noreply.github.com>
2021-02-23 23:19:26 -0300

    a
    a

--- dInventario.cpp
+++ dInventario.cpp
@@ -12,20 +12,25 @@ dInventario::dInventario(wxWindow *parent, Personaje &p, Partida *pa) : Dialogo_
 
 void dInventario::OnClickAgregar( wxCommandEvent& event )  {
 	int pos=m_ListaItems->GetSelection();
-	Item I=m_partida->ObtenerItem(pos);
-	Cant=m_Personaje->TamInv();
-	bool esigual=false;
-	for(int i=0;i<Cant;i++) { 
-		Item I2=m_Personaje->MostrarItem(i);
-		if(I==I2){ 
-			wxMessageBox(wxT("No es posible agregar un item que ya exista\nen el inventario del personaje."),wxT("Error"),wxICON_ERROR);
-			esigual=true;
+	if(pos==wxNOT_FOUND){
+		wxMessageBox(wxT("No es posible agregar un item sin\nhaber seleccionado uno previamente."),wxT("Error"),wxICON_ERROR);
+	} else {
+		Item I=m_partida->ObtenerItem(pos);
+		Cant=m_Personaje->TamInv();
+		bool esigual=false;
+		for(int i=0;i<Cant;i++) { 
+			Item I2=m_Personaje->MostrarItem(i);
+			if(I==I2){ 
+				wxMessageBox(wxT("No es posible agregar un item que ya exista\nen el inventario del personaje."),wxT("Error"),wxICON_ERROR);
+				esigual=true;
+			}
+		}
+		if(!esigual){ 
+			m_Personaje->AgregarInv(I);
+			EndModal(1);
 		}
 	}
-	if(!esigual){ 
-		m_Personaje->AgregarInv(I);
-		EndModal(1);
-	}
+
 }
 
 void dInventario::OnClickCancelar( wxCommandEvent& event )  {
