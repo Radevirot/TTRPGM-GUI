edf60306889febb92efe26df68f343956cfe6c59
Radevirot <78053632+Radevirot@users.noreply.github.com>
2021-02-27 16:28:28 -0300

    a
    a

--- dItem.cpp
+++ dItem.cpp
@@ -1,65 +1,54 @@
 #include "dItem.h"
 
 dItem::dItem(wxWindow *parent, Partida *p, Item Im, int posc) : Dialogo_item(parent) {
+	CrearTablaDeAtajos();
+	
 	m_partida=p;
-	I=Im;
-	pos=posc;
-	m_NombreItem->SetLabel(std_to_wx(I.ObtenerNombre()));
-	m_Cantidad->SetValue(std_to_wx(std::to_string(I.ObtenerCant())));
-	m_PuntosVida->SetValue(std_to_wx(std::to_string(I.ObtenerStat(0)))); 
-	m_Defenza->SetValue(std_to_wx(std::to_string(I.ObtenerStat(1)))); 
-	m_Fuerza->SetValue(std_to_wx(std::to_string(I.ObtenerStat(2)))); 
-	m_Agilidad->SetValue(std_to_wx(std::to_string(I.ObtenerStat(3)))); 
-	m_ResistenciaMagica->SetValue(std_to_wx(std::to_string(I.ObtenerStat(4))));
-	m_Inteligencia->SetValue(std_to_wx(std::to_string(I.ObtenerStat(5))));
-	m_Mana->SetValue(std_to_wx(std::to_string(I.ObtenerStat(6))));
-	m_Danio->SetValue(std_to_wx(std::to_string(I.ObtenerStat(7)))); 
-	m_Rango->SetValue(std_to_wx(std::to_string(I.ObtenerStat(8)))); 
-	m_Punteria->SetValue(std_to_wx(std::to_string(I.ObtenerStat(9)))); 
-	m_Bloqueo->SetValue(std_to_wx(std::to_string(I.ObtenerStat(10)))); 
-	m_Detalle->SetLabel(std_to_wx(I.ObtenerDetalle())); 
-	m_Descripcion->SetLabel(std_to_wx(I.ObtenerDesc()));
+	I=Im; pos=posc;
+	
+	ObtenerInformacion(I);
 	Show();
 }
 
 dItem::dItem(wxWindow *parent, Partida *p, Item &Im, int posc, bool Uso) : Dialogo_item(parent) {
+	CrearTablaDeAtajos();
+	
 	m_partida=p;
-	pos=posc;
-	Ims=&Im;
-	I=Im;
-	booleano=Uso;
-	m_NombreItem->SetLabel(std_to_wx(Im.ObtenerNombre()));
-	m_Cantidad->SetValue(std_to_wx(std::to_string(Im.ObtenerCant())));
-	m_PuntosVida->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(0)))); 
-	m_Defenza->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(1)))); 
-	m_Fuerza->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(2)))); 
-	m_Agilidad->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(3)))); 
-	m_ResistenciaMagica->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(4))));
-	m_Inteligencia->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(5))));
-	m_Mana->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(6))));
-	m_Danio->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(7)))); 
-	m_Rango->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(8)))); 
-	m_Punteria->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(9)))); 
-	m_Bloqueo->SetValue(std_to_wx(std::to_string(Im.ObtenerStat(10)))); 
-	m_Detalle->SetLabel(std_to_wx(Im.ObtenerDetalle())); 
-	m_Descripcion->SetLabel(std_to_wx(Im.ObtenerDesc()));
+	I=Im; pos=posc;
+	Ims=&Im; booleano=Uso;
+	
+	ObtenerInformacion(Im);
 	Show();
+}
+
+void dItem::CrearTablaDeAtajos(){
+	wxAcceleratorEntry entries[3];
+	entries[0].Set(wxACCEL_CTRL,(int) 'S', wxID_HIGHEST+17);
+	entries[1].Set(wxACCEL_CTRL,(int) 'G', wxID_HIGHEST+18);
+	entries[2].Set(wxACCEL_NORMAL,WXK_RETURN, wxID_HIGHEST+19);
+	wxAcceleratorTable accel(3, entries);
+	SetAcceleratorTable(accel);
+	Connect( wxID_HIGHEST+17 , wxEVT_MENU, wxCommandEventHandler( dItem::OnClickExportar ));
+	Connect( wxID_HIGHEST+18 , wxEVT_MENU, wxCommandEventHandler( dItem::OnClickExportar ));
+	Connect( wxID_HIGHEST+19 , wxEVT_MENU, wxCommandEventHandler( dItem::OnClickAplicar ));
+}
+
+void dItem::ObtenerInformacion(Item &Ite){
+	m_NombreItem->SetLabel(std_to_wx(Ite.ObtenerNombre()));
+	m_Cantidad->SetValue(std_to_wx(std::to_string(Ite.ObtenerCant())));
+	for(size_t i=0;i<vect.size();i++) { 
+		vect[i]->SetValue(std_to_wx(std::to_string(Ite.ObtenerStat(i)))); 
+	}
+	m_Detalle->SetLabel(std_to_wx(Ite.ObtenerDetalle())); 
+	m_Descripcion->SetLabel(std_to_wx(Ite.ObtenerDesc()));
 }
 
 void dItem::ActualizarInformacion(Item &Ite){
 	Ite.NombrarItem(wx_to_std(m_NombreItem->GetValue()));
 	Ite.ModificarCant(m_Cantidad->GetValue());
-	Ite.ModificarStat(0,(m_PuntosVida->GetValue()));
-	Ite.ModificarStat(1,(m_Defenza->GetValue()));
-	Ite.ModificarStat(2,(m_Fuerza->GetValue()));
-	Ite.ModificarStat(3,(m_Agilidad->GetValue()));
-	Ite.ModificarStat(4,(m_ResistenciaMagica->GetValue()));
-	Ite.ModificarStat(5,(m_Inteligencia->GetValue()));
-	Ite.ModificarStat(6,(m_Mana->GetValue()));
-	Ite.ModificarStat(7,(m_Danio->GetValue()));
-	Ite.ModificarStat(8,(m_Rango->GetValue()));
-	Ite.ModificarStat(9,(m_Punteria->GetValue()));
-	Ite.ModificarStat(10,(m_Bloqueo->GetValue()));
+	for(size_t i=0;i<vect.size();i++) { 
+		Ite.ModificarStat(i,(vect[i]->GetValue()));
+	}
 	Ite.ModificarDetalle(wx_to_std(m_Detalle->GetValue()));
 	Ite.ModificarDesc(wx_to_std(m_Descripcion->GetValue()));
 }
@@ -70,12 +59,12 @@ void dItem::ActualizarInformacion(Item &Ite){
 void dItem::OnClickAplicar( wxCommandEvent& event )  {
 
 	if(booleano){
-		this->ActualizarInformacion(I);
+		ActualizarInformacion(I);
 		m_partida->EliminarItem(pos);
 		m_partida->AgregarItem(I);
 		EndModal(1);
 	}else{
-		this->ActualizarInformacion(*Ims);
+		ActualizarInformacion(*Ims);
 		EndModal(1);
 	}
 	
@@ -84,7 +73,7 @@ void dItem::OnClickAplicar( wxCommandEvent& event )  {
 void dItem::OnClickExportar( wxCommandEvent& event )  {
 	wxFileDialog exportarItem(this,wxT("Exportar item"),".\\datos",m_NombreItem->GetValue()+".ite","Archivos ITE (*.ite)|*.ite",wxFD_SAVE|wxFD_OVERWRITE_PROMPT);
 	if(exportarItem.ShowModal()==wxID_OK){
-		this->ActualizarInformacion(I);
+		ActualizarInformacion(I);
 		I.Exportar(wx_to_std(exportarItem.GetPath()));
 	}
 }
--- dItem.h
+++ dItem.h
@@ -13,8 +13,12 @@ private:
 	Partida *m_partida;
 	Item I, *Ims;
 	int pos;
-	void ActualizarInformacion(Item &Ite);
 	bool booleano=true;
+	std::vector<wxSpinCtrlDouble*> vect{m_PuntosVida,m_Defenza,m_Fuerza,m_Agilidad,m_ResistenciaMagica,m_Inteligencia,m_Mana,m_Danio,m_Rango,m_Punteria,m_Bloqueo};
+	void CrearTablaDeAtajos();
+	void ActualizarInformacion(Item &Ite);
+	void ObtenerInformacion(Item &Ite);
+	
 protected:
 	
 	//BOTONES DE EDICION DE ITEM
